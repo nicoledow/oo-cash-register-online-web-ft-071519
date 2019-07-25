34062f99a3bdc0825aae81f48082a762a1669860
Nicole Dow <nikki.dow71@gmail.com>
2019-07-25 16:36:14 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
+=begin
+We're going to create an Object Oriented Cash Register that can:
 
+Add items of varying quantities and prices
+Calculate discounts
+Keep track of what's been added to it
+Void the last transaction
+=end
+
+class CashRegister
+  attr_accessor :total, :discount
+  attr_reader :items
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @last_total = total
+    @total += (price * quantity)
+    quantity.times do
+      @items << title
+    end
+  end
+  
+  def apply_discount
+    @total -= (total * discount / 100)
+    if discount != 0
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+  
+  def void_last_transaction
+    @total = @last_total
+  end
+  
+  
+end
